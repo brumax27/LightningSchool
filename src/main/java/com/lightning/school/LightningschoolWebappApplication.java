ba770458e0be9779b0123ad122ea50b413b2f1b2
MTrembley <tit-max@live.fr>
2019-02-19 14:44:28 +0100

    projet finit

---
+++ src/main/java/com/lightning/school/LightningschoolWebappApplication.java
@@ -0,0 +1,13 @@
+package com.lightning.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightningschoolWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightningschoolWebappApplication.class, args);
+    }
+
+}
