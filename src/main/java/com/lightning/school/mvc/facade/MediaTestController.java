981b32b4fd286b726fa38a71692bfc39640074b8
brumax27 <tit-max@live.fr>
2019-05-24 09:59:12 +0200

    controlleur test

--- src/main/java/com/lightning/school/mvc/facade/MediaTestController.java
+++ src/main/java/com/lightning/school/mvc/facade/MediaTestController.java
@@ -3,8 +3,8 @@ package com.lightning.school.mvc.facade;
 import com.lightning.school.mvc.delegate.aws.MediaStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,7 +16,7 @@ public class MediaTestController {
     @Autowired
     private MediaStoreService mediaStoreService;
 
-    @RequestMapping(value = "", headers = "Content-Type= multipart/form-data", method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity uploadFile(@RequestParam("file") MultipartFile file) {
        String urlFile = mediaStoreService.putMedia(file);
        return ResponseEntity.ok(urlFile);
