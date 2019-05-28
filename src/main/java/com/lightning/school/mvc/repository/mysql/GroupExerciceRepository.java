db47c5360f4b46e08e9832f3784dcaf26a038393
brumax27 <tit-max@live.fr>
2019-05-28 17:42:44 +0200

    remove element

--- src/main/java/com/lightning/school/mvc/repository/mysql/GroupExerciceRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.lightning.school.mvc.repository.mysql;
-//
-//import com.lightning.school.mvc.model.GroupExercice;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface GroupExerciceRepository extends JpaRepository<GroupExercice, Integer> {
-//}
--- src/main/java/com/lightning/school/mvc/repository/mysql/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.lightning.school.mvc.repository.mysql;
-//
-//import com.lightning.school.mvc.model.Group;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface GroupRepository extends JpaRepository<Group, Integer> {
-//}
