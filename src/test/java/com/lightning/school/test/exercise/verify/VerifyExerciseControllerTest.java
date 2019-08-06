623974005103229eb5523a5155541feee4fcff94
Brumax27 <tit-max@live.fr>
2019-08-06 11:40:57 +0200

    https to http

--- src/test/java/com/lightning/school/test/exercise/verify/VerifyExerciseControllerTest.java
+++ src/test/java/com/lightning/school/test/exercise/verify/VerifyExerciseControllerTest.java
@@ -10,22 +10,24 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class VerifyExerciseControllerTest {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
 
+    @LocalServerPort
+    private int port;
     private String token;
 
     @Before
-    public void init() {
-        RestAssured.port = 8554;
-        RestAssured.baseURI = "https://localhost";
+    public void init(){
+        RestAssured.port = port;
         connect();
     }
 
@@ -35,7 +37,7 @@ public class VerifyExerciseControllerTest {
         dto.setPassword("admin");
 
         String location =
-                given().relaxedHTTPSValidation()
+                given()
                         .log().all()
                         .contentType(ContentType.JSON)
                         .body(dto)
@@ -54,7 +56,7 @@ public class VerifyExerciseControllerTest {
     public void controllerTest(){
         VerifyExoIn in = new VerifyExoIn("1!10|&", null, 3,5);
 
-        ResultExerciseOut resultExerciseOut = given().relaxedHTTPSValidation().log().all().contentType(ContentType.JSON).header(AUTHORIZATION_HEADER, token)
+        ResultExerciseOut resultExerciseOut = given().log().all().contentType(ContentType.JSON).header(AUTHORIZATION_HEADER, token)
                 .body(in).when().post("/api/verify/exercise").then().log().all().statusCode(200).extract().as(ResultExerciseOut.class);
 
         Assert.assertFalse(resultExerciseOut.getResultExercice());
